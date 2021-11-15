da7430d33848993269189606862aa6ac287305ad
Antonis Geralis <b3lievem@yandex.com>
2021-11-15 13:03:53 +0200

    some examples

---
+++ collision_area.nim
@@ -0,0 +1,97 @@
+# **************************************************************************************
+#
+# raylib [shapes] example - collision area
+#
+# This example has been created using raylib 2.5 (www.raylib.com)
+# raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+#
+# Copyright (c) 2013-2019 Ramon Santamaria (@raysan5)
+#
+# **************************************************************************************
+
+import nimraylib_now, std/lenientops
+
+proc main =
+  # Initialization
+  # ---------------------------------------------------------
+  let screenWidth = 800'i32
+  let screenHeight = 450'i32
+  initWindow(screenWidth, screenHeight, "raylib [shapes] example - collision area")
+  # Box A: Moving box
+  var boxA = Rectangle(
+    x: 10,
+    y: getScreenHeight().float32 / 2 - 50,
+    width: 200,
+    height: 100
+  )
+  var boxASpeedX = 4'i32
+  # Box B: Mouse moved box
+  var boxB = Rectangle(
+    x: getScreenWidth().float32 / 2 - 30,
+    y: getScreenHeight().float32 / 2 - 30,
+    width: 60,
+    height: 60
+  )
+  var boxCollision: Rectangle
+  # Collision rectangle
+  var screenUpperLimit = 40'i32
+  # Top menu limits
+  var pause = false
+  # Movement pause
+  var collision = false
+  # Collision detection
+  setTargetFPS(60)
+  # Set our game to run at 60 frames-per-second
+  # ----------------------------------------------------------
+  # Main game loop
+  while not windowShouldClose(): # Detect window close button or ESC key
+    # Update
+    # -----------------------------------------------------
+    # Move box if not paused
+    if not pause:
+      boxA.x += boxASpeedX.float32
+    if boxA.x + boxA.width >= getScreenWidth() or boxA.x <= 0:
+      boxASpeedX = boxASpeedX * -1
+    boxB.x = getMouseX() - boxB.width / 2
+    boxB.y = getMouseY() - boxB.height / 2
+    # Make sure Box B does not go out of move area limits
+    if boxB.x + boxB.width >= getScreenWidth():
+      boxB.x = getScreenWidth() - boxB.width
+    elif boxB.x <= 0:
+      boxB.x = 0
+    if boxB.y + boxB.height >= getScreenHeight():
+      boxB.y = getScreenHeight() - boxB.height
+    elif boxB.y <= screenUpperLimit: # Check boxes collision
+      boxB.y = screenUpperLimit.float32
+    collision = checkCollisionRecs(boxA, boxB)
+    # Get collision rectangle (only on collision)
+    if collision:
+      boxCollision = getCollisionRec(boxA, boxB)
+    if isKeyPressed(Space):
+      pause = not pause
+    beginDrawing()
+    clearBackground(White)
+    drawRectangle(0, 0, screenWidth, screenUpperLimit, if collision: Red else: Black)
+    drawRectangleRec(boxA, Gold)
+    drawRectangleRec(boxB, Blue)
+    if collision:
+      # Draw collision area
+      drawRectangleRec(boxCollision, Lime)
+      # Draw collision message
+      drawText("COLLISION!",
+          getScreenWidth() div 2 - measureText("COLLISION!", 20) div 2,
+          screenUpperLimit div 2 - 10, 20, Black)
+      # Draw collision area
+      drawText(textFormat("Collision Area: %i", boxCollision.width.int32 *
+          boxCollision.height.int32), getScreenWidth() div 2 - 100,
+          screenUpperLimit + 10, 20, Black)
+    drawFPS(10, 10)
+    endDrawing()
+    # -----------------------------------------------------
+  # De-Initialization
+  # ---------------------------------------------------------
+  closeWindow()
+  # Close window and OpenGL context
+  # ----------------------------------------------------------
+
+main()
---
+++ platformer.nim
@@ -0,0 +1,259 @@
+# **************************************************************************************
+#
+# raylib [core] example - 2d camera platformer
+#
+# This example has been created using raylib 2.5 (www.raylib.com)
+# raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+#
+# Example contributed by arvyy (@arvyy) and reviewed by Ramon Santamaria (@raysan5)
+#
+# Copyright (c) 2019 arvyy (@arvyy)
+# Converted in 2021 by greenfork
+#
+# **************************************************************************************
+
+import std/lenientops, nimraylib_now
+
+const
+  G = 400
+  PlayerJumpSpd = 350'f32
+  PlayerHorSpd = 200'f32
+
+type
+  Player = object
+    position: Vector2
+    speed: float32
+    canJump: bool
+
+  EnvItem = object
+    rect: Rectangle
+    blocking: bool
+    color: Color
+
+proc updatePlayer(player: var Player, envItems: openArray[EnvItem],
+    delta: float32)
+proc updateCameraCenter(camera: var Camera2D, player: Player,
+                        envItems: openArray[EnvItem], delta: float32,
+                        width: int32, height: int32)
+proc updateCameraCenterInsideMap(camera: var Camera2D, player: Player,
+                                 envItems: openArray[EnvItem],
+                                 delta: float32, width: int32, height: int32)
+proc updateCameraCenterSmoothFollow(camera: var Camera2D, player: Player,
+                                    envItems: openArray[EnvItem],
+                                    delta: float32, width: int32, height: int32)
+proc updateCameraEvenOutOnLanding(camera: var Camera2D, player: Player,
+                                  envItems: openArray[EnvItem],
+                                  delta: float32, width: int32, height: int32)
+proc updateCameraPlayerBoundsPush(camera: var Camera2D, player: Player,
+                                  envItems: openArray[EnvItem],
+                                  delta: float32, width: int32, height: int32)
+# Initialization
+# --------------------------------------------------------------------------------------
+let screenWidth = 800'i32
+let screenHeight = 450'i32
+initWindow(screenWidth, screenHeight, "raylib [core] example - 2d camera")
+var player = Player(
+  position: Vector2(x: 400, y: 280),
+  speed: 0,
+  canJump: false
+)
+let envItems = [
+  EnvItem(rect: Rectangle(x: 0, y: 0, width: 1000, height: 400), blocking: false,
+      color: Lightgray),
+  EnvItem(rect: Rectangle(x: 0, y: 400, width: 1000, height: 200),
+      blocking: true, color: Gray),
+  EnvItem(rect: Rectangle(x: 300, y: 200, width: 400, height: 10),
+      blocking: true, color: Gray),
+  EnvItem(rect: Rectangle(x: 250, y: 300, width: 100, height: 10),
+      blocking: true, color: Gray),
+  EnvItem(rect: Rectangle(x: 650, y: 300, width: 100, height: 10),
+      blocking: true, color: Gray),
+]
+var camera = Camera2D(
+  target: player.position,
+  offset: Vector2(x: screenWidth.float32 / 2, y: screenHeight.float32 / 2),
+  rotation: 0,
+  zoom: 1
+)
+# Store multiple update camera functions
+let cameraUpdaters = [
+  updateCameraCenter,
+  updateCameraCenterInsideMap,
+  updateCameraCenterSmoothFollow,
+  updateCameraEvenOutOnLanding,
+  updateCameraPlayerBoundsPush,
+]
+var cameraOption = 0
+let cameraDescriptions = ["Follow player center", "Follow player center, but clamp to map edges",
+                          "Follow player center; smoothed",
+                          "Follow player center horizontally; updateplayer center vertically after landing",
+                          "Player push camera on getting too close to screen edge"]
+setTargetFPS(60)
+# -------------------------------------------------------------------------------------
+# Main game loop
+while not windowShouldClose():
+  # Update
+  # ----------------------------------------------------------------------------------
+  let deltaTime = getFrameTime()
+  updatePlayer(player, envItems, deltaTime)
+  camera.zoom += getMouseWheelMove() * 0.05
+  if camera.zoom > 3:
+    camera.zoom = 3.0
+  elif camera.zoom < 0.25:
+    camera.zoom = 0.25
+  if isKeyPressed(R):
+    camera.zoom = 1.0
+    player.position = Vector2(x: 400, y: 280)
+  if isKeyPressed(C):
+    cameraOption = (cameraOption + 1) mod cameraUpdaters.len
+  cameraUpdaters[cameraOption](camera, player, envItems,
+                               deltaTime, screenWidth,
+                               screenHeight)
+  # ---------------------------------------------------------------------------------
+  # Draw
+  # ---------------------------------------------------------------------------------
+  beginDrawing()
+  clearBackground(Lightgray)
+  beginMode2D(camera)
+  for i in 0 ..< envItems.len:
+    drawRectangleRec(envItems[i].rect, envItems[i].color)
+  let playerRect = Rectangle(x: player.position.x - 20, y: player.position.y - 40,
+      width: 40, height: 40)
+  drawRectangleRec(playerRect, Red)
+  endMode2D()
+  drawText("Controls:", 20, 20, 10, Black)
+  drawText("- Right/Left to move", 40, 40, 10, Darkgray)
+  drawText("- Space to jump", 40, 60, 10, Darkgray)
+  drawText("- Mouse Wheel to Zoom in-out, R to reset zoom", 40, 80, 10, Darkgray)
+  drawText("- C to change camera mode", 40, 100, 10, Darkgray)
+  drawText("Current camera mode:", 20, 120, 10, Black)
+  drawText(cameraDescriptions[cameraOption].cstring, 40, 140, 10, Darkgray)
+  endDrawing()
+
+# ---------------------------------------------------------------------------------
+# De-Initialization
+# -------------------------------------------------------------------------------------
+closeWindow()
+# Close window and OpenGL context
+# -------------------------------------------------------------------------------------
+
+proc updatePlayer(player: var Player, envItems: openArray[EnvItem],
+    delta: float32) =
+  if isKeyDown(Left):
+    player.position.x -= PlayerHorSpd * delta
+  if isKeyDown(Right):
+    player.position.x += PlayerJumpSpd * delta
+  if isKeyDown(Space) and player.canJump:
+    player.speed = -PlayerJumpSpd
+    player.canJump = false
+  var hitObstacle = false
+  for i in 0 ..< envItems.len:
+    let ei = envItems[i]
+    let p = player.position
+    if ei.blocking and ei.rect.x <= p.x and ei.rect.x + ei.rect.width >= p.x and
+        ei.rect.y >= p.y and ei.rect.y < p.y + player.speed * delta:
+      hitObstacle = true
+      player.speed = 0
+      player.position.y = ei.rect.y
+  if not hitObstacle:
+    player.position.y += player.speed * delta
+    player.speed += G * delta
+    player.canJump = false
+  else:
+    player.canJump = true
+
+proc updateCameraCenter(camera: var Camera2D, player: Player,
+                        envItems: openArray[EnvItem], delta: float32,
+                        width: int32, height: int32) =
+  camera.offset = Vector2(x: width.float32 / 2, y: height.float32 / 2)
+  camera.target = player.position
+
+proc updateCameraCenterInsideMap(camera: var Camera2D, player: Player,
+                                 envItems: openArray[EnvItem],
+                                 delta: float32, width: int32, height: int32) =
+  camera.target = player.position
+  camera.offset = Vector2(x: width.float32 / 2, y: height.float32 / 2)
+  var
+    minX = 1000'f32
+    minY = 1000'f32
+    maxX = -1000'f32
+    maxY = -1000'f32
+  for i in 0 ..< envItems.len:
+    let ei = envItems[i]
+    minX = min(ei.rect.x, minX)
+    maxX = max(ei.rect.x + ei.rect.width, maxX)
+    minY = min(ei.rect.y, minY)
+    maxY = max(ei.rect.y + ei.rect.height, maxY)
+  let
+    maxV = getWorldToScreen2D(Vector2(x: maxX, y: maxY), camera)
+    minV = getWorldToScreen2D(Vector2(x: minX, y: minY), camera)
+  if maxV.x < width:
+    camera.offset.x = width - (maxV.x - width.float32 / 2)
+  if maxV.y < height:
+    camera.offset.y = height - (maxV.y - height.float32 / 2)
+  if minV.x > 0:
+    camera.offset.x = width.float32 / 2 - minV.x
+  if minV.y > 0:
+    camera.offset.y = height.float32 / 2 - minV.y
+
+proc updateCameraCenterSmoothFollow(camera: var Camera2D, player: Player,
+                                    envItems: openArray[EnvItem],
+                                    delta: float32, width: int32, height: int32) =
+  let minSpeed = 30'f32
+  let minEffectLength = 10'f32
+  let fractionSpeed = 0.8'f32
+  camera.offset = Vector2(x: width.float32 / 2, y: height.float32 / 2)
+  let diff = subtract(player.position, camera.target)
+  let length = length(diff)
+  if length > minEffectLength:
+    let speed = max(fractionSpeed * length, minSpeed)
+    camera.target = add(camera.target, scale(diff, speed * delta / length))
+
+var eveningOut = false
+var evenOutTarget: float32
+proc updateCameraEvenOutOnLanding(camera: var Camera2D, player: Player,
+                                  envItems: openArray[EnvItem],
+                                  delta: float32, width: int32, height: int32) =
+  let evenOutSpeed = 700'f32
+  camera.offset = Vector2(x: width.float32 / 2, y: height.float32 / 2)
+  camera.target.x = player.position.x
+  if eveningOut:
+    if evenOutTarget > camera.target.y:
+      camera.target.y += evenOutSpeed * delta
+      if camera.target.y > evenOutTarget:
+        camera.target.y = evenOutTarget
+        eveningOut = false
+    else:
+      camera.target.y -= evenOutSpeed * delta
+      if camera.target.y < evenOutTarget:
+        camera.target.y = evenOutTarget
+        eveningOut = false
+  else:
+    if player.canJump and player.speed == 0 and
+       player.position.y != camera.target.y:
+      eveningOut = true
+      evenOutTarget = player.position.y
+
+proc updateCameraPlayerBoundsPush(camera: var Camera2D, player: Player,
+                                  envItems: openArray[EnvItem],
+                                  delta: float32, width: int32, height: int32) =
+  let
+    bbox = Vector2(x: 0.2, y: 0.2)
+    bboxWorldMin = getScreenToWorld2D(
+      Vector2(x: (1 - bbox.x) * 0.5'f32 * width, y: (1 - bbox.y) * 0.5'f32 * height),
+      camera
+    )
+    bboxWorldMax = getScreenToWorld2D(
+      Vector2(x: (1 + bbox.x) * 0.5'f32 * width, y: (1 + bbox.y) * 0.5'f32 * height),
+      camera
+    )
+  camera.offset = Vector2(x: (1 - bbox.x) * 0.5'f32 * width, y: (1 -
+      bbox.y) * 0.5'f32 * height)
+  if player.position.x < bboxWorldMin.x:
+    camera.target.x = player.position.x
+  if player.position.y < bboxWorldMin.y:
+    camera.target.y = player.position.y
+  if player.position.x > bboxWorldMax.x:
+    camera.target.x = bboxWorldMin.x + (player.position.x - bboxWorldMax.x)
+  if player.position.y > bboxWorldMax.y:
+    camera.target.y = bboxWorldMin.y + (player.position.y - bboxWorldMax.y)
