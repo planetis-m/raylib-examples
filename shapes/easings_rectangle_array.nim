0af46fdd5cddde0aafabbd1c3831a2ea0d14b40b
Antonis Geralis <b3lievem@yandex.com>
2022-11-27 13:00:09 +0200

    add example

---
+++ shapes/easings_rectangle_array.nim
@@ -0,0 +1,91 @@
+# ******************************************************************************************
+#
+#   raylib [shapes] example - easings rectangle array
+#
+#   NOTE: This example requires 'easings.h' library, provided on raylib/src. Just copy
+#   the library to same directory as example or make sure it's available on include path.
+#
+#   Example originally created with raylib 2.0, last time updated with raylib 2.5
+#
+#   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+#   BSD-like license that allows static linking with closed source software
+#
+#   Copyright (c) 2014-2022 Ramon Santamaria (@raysan5)
+#
+# ******************************************************************************************
+
+import raylib, reasings, std/lenientops
+
+const
+  screenWidth = 800
+  screenHeight = 450
+
+const
+  RecsWidth = 50
+  RecsHeight = 50
+  MaxRecsX = 800 div RecsWidth
+  MaxRecsY = 450 div RecsHeight
+  PlayTimeInFrames = 240 # At 60 fps = 4 seconds
+
+# ------------------------------------------------------------------------------------
+# Program main entry point
+# ------------------------------------------------------------------------------------
+
+proc main =
+  # Initialization
+  # --------------------------------------------------------------------------------------
+  initWindow(screenWidth, screenHeight, "raylib [shapes] example - easings rectangle array")
+  var recs: array[MaxRecsX * MaxRecsY, Rectangle]
+  for y in 0..<MaxRecsY:
+    for x in 0..<MaxRecsX:
+      recs[y * MaxRecsX + x].x = RecsWidth / 2'f32 + RecsWidth * x
+      recs[y * MaxRecsX + x].y = RecsHeight / 2'f32 + RecsHeight * y
+      recs[y * MaxRecsX + x].width = RecsWidth
+      recs[y * MaxRecsX + x].height = RecsHeight
+  var rotation: float32 = 0
+  var framesCounter: int32 = 0
+  var state: int32 = 0 # Rectangles animation state: 0-Playing, 1-Finished
+  setTargetFPS(60) # Set our game to run at 60 frames-per-second
+  # --------------------------------------------------------------------------------------
+  # Main game loop
+  while not windowShouldClose(): # Detect window close button or ESC key
+    # Update
+    # ----------------------------------------------------------------------------------
+    if state == 0:
+      inc(framesCounter)
+      for i in 0 ..< MaxRecsX * MaxRecsY:
+        recs[i].height = circOut(framesCounter.float32, RecsHeight, -RecsHeight, PlayTimeInFrames)
+        recs[i].width = circOut(framesCounter.float32, RecsWidth, -RecsWidth, PlayTimeInFrames)
+        if recs[i].height < 0:
+          recs[i].height = 0
+        if recs[i].width < 0:
+          recs[i].width = 0
+        if recs[i].height == 0 and recs[i].width == 0:
+          state = 1
+        rotation = linearIn(framesCounter.float32, 0, 360, PlayTimeInFrames)
+    elif state == 1 and isKeyPressed(KeySpace):
+      # ----------------------------------------------------------------------------------
+      # Draw
+      # ----------------------------------------------------------------------------------
+      # When animation has finished, press space to restart
+      framesCounter = 0
+      for i in 0 ..< MaxRecsX * MaxRecsY:
+        recs[i].height = RecsHeight
+        recs[i].width = RecsWidth
+      state = 0
+    beginDrawing()
+    clearBackground(RayWhite)
+    if state == 0:
+      for i in 0 ..< MaxRecsX * MaxRecsY:
+        drawRectanglePro(recs[i], Vector2(x: recs[i].width / 2, y: recs[i].height / 2), rotation, Red)
+    elif state == 1:
+      drawText("PRESS [SPACE] TO PLAY AGAIN!", 240, 200, 20, Gray)
+    endDrawing()
+    # ------------------------------------------------------------------------------------
+  # De-Initialization
+  # --------------------------------------------------------------------------------------
+  closeWindow()
+  # Close window and OpenGL context
+  # --------------------------------------------------------------------------------------
+
+main()
