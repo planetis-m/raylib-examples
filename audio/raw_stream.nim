462ce1905145bf19af6d4e5de58efdb5f1ae28f3
Antonis Geralis <b3lievem@yandex.com>
2023-01-30 01:50:00 +0200

    nah it was broken

--- audio/raw_stream.nim
+++ audio/raw_stream.nim
@@ -23,6 +23,24 @@ const
   MaxSamples = 512
   MaxSamplesPerUpdate = 4096
 
+var
+  frequency: float32 = 440 # Cycles per second (hz)
+  audioFrequency: float32 = 440 # Audio frequency, for smoothing
+  oldFrequency: float32 = 1 # Previous value, used to test if sine needs to be rewritten, and to smoothly modulate frequency
+  sineIdx: float32 = 0 # Index for audio rendering
+
+proc audioInputCallback(buffer: pointer; frames: uint32) {.cdecl.} =
+  # Audio input processing callback
+  audioFrequency = frequency + (audioFrequency - frequency)*0.95'f32
+  audioFrequency += 1
+  audioFrequency -= 1
+  let incr = audioFrequency/44100'f32
+  let d = cast[ptr UncheckedArray[int16]](buffer)
+  for i in 0..<frames:
+    d[i] = int16(32000'f32*sin(2*PI*sineIdx))
+    sineIdx += incr
+    if sineIdx > 1: sineIdx -= 1
+
 # ----------------------------------------------------------------------------------------
 # Program main entry point
 # ----------------------------------------------------------------------------------------
@@ -39,6 +57,7 @@ proc main =
   setAudioStreamBufferSizeDefault(MaxSamplesPerUpdate)
   # Init raw audio stream (sample rate: 44100, sample size: 16bit-short, channels: 1-mono)
   var stream = loadAudioStream(44100, 16, 1)
+  setAudioStreamCallback(stream, audioInputCallback)
   # Buffer for the single cycle waveform we are synthesizing
   var data = newSeq[int16](MaxSamples)
   # Frame buffer, describing the waveform when repeated over the course of a frame
@@ -47,12 +66,12 @@ proc main =
   # Start processing stream buffer (no data loaded currently)
   # Position read in to determine next frequency
   var mousePosition = Vector2(x: -100, y: -100)
-  # Cycles per second (hz)
-  var frequency: float32 = 440
-  # Previous value, used to test if sine needs to be rewritten, and to smoothly modulate frequency
-  var oldFrequency: float32 = 1
-  # Cursor to read and copy the samples of the sine wave buffer
-  var readCursor: int32 = 0
+  # # Cycles per second (hz)
+  # var frequency: float32 = 440
+  # # Previous value, used to test if sine needs to be rewritten, and to smoothly modulate frequency
+  # var oldFrequency: float32 = 1
+  # # Cursor to read and copy the samples of the sine wave buffer
+  # var readCursor: int32 = 0
   # Computed size in samples of the sine wave
   var waveLength: int32 = 1
   var position: Vector2
@@ -72,7 +91,7 @@ proc main =
       setAudioStreamPan(stream, pan)
     if frequency != oldFrequency:
       # Compute wavelength. Limit size in both directions.
-      let oldWavelength = waveLength
+      # let oldWavelength = waveLength
       waveLength = int32(22050/frequency)
       if waveLength > MaxSamples div 2: waveLength = MaxSamples div 2
       if waveLength < 1: waveLength = 1
@@ -82,24 +101,24 @@ proc main =
       for j in waveLength*2..<MaxSamples:
         data[j] = int16(0)
       # Scale read cursor's position to minimize transition artifacts
-      readCursor = int32(readCursor.float32*(waveLength.float32/oldWavelength.float32))
+      # readCursor = int32(readCursor.float32*(waveLength.float32/oldWavelength.float32))
       oldFrequency = frequency
-    # Refill audio stream if required
-    if isAudioStreamProcessed(stream):
-      # Synthesize a buffer that is exactly the requested size
-      var writeCursor: int32 = 0
-      while writeCursor < MaxSamplesPerUpdate:
-        # Start by trying to write the whole chunk at once
-        var writeLength = MaxSamplesPerUpdate - writeCursor
-        # Limit to the maximum readable size
-        let readLength = waveLength - readCursor
-        if writeLength > readLength: writeLength = readLength
-        copyMem(addr writeBuf[writeCursor], addr data[readCursor], writeLength*sizeof(int16))
-        # Update cursors and loop audio
-        readCursor = (readCursor + writeLength) mod waveLength
-        inc writeCursor, writeLength
-      # Copy finished frame to audio stream
-      updateAudioStream(stream, writeBuf)
+    # # Refill audio stream if required
+    # if isAudioStreamProcessed(stream):
+    #   # Synthesize a buffer that is exactly the requested size
+    #   var writeCursor: int32 = 0
+    #   while writeCursor < MaxSamplesPerUpdate:
+    #     # Start by trying to write the whole chunk at once
+    #     var writeLength = MaxSamplesPerUpdate - writeCursor
+    #     # Limit to the maximum readable size
+    #     let readLength = waveLength - readCursor
+    #     if writeLength > readLength: writeLength = readLength
+    #     copyMem(addr writeBuf[writeCursor], addr data[readCursor], writeLength*sizeof(int16))
+    #     # Update cursors and loop audio
+    #     readCursor = (readCursor + writeLength) mod waveLength
+    #     inc writeCursor, writeLength
+    #   # Copy finished frame to audio stream
+    #   updateAudioStream(stream, writeBuf)
     # ------------------------------------------------------------------------------------
     # Draw
     # ------------------------------------------------------------------------------------
