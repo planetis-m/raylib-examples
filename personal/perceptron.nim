cd3de6b1dfe24f379d63219e8af08d72b1723f95
Antonis Geralis <b3lievem@yandex.com>
2024-02-06 21:40:22 +0200

    improve perceptron example, flips y-axis successfully

--- personal/perceptron.nim
+++ personal/perceptron.nim
@@ -31,7 +31,7 @@ proc initPerceptron(lr: float32): Perceptron =
   # Initialize the perceptron with random weights and a given learning rate
   result = Perceptron(lr: lr)
   for i in 0 ..< 3:
-    result.weights[i] = rand(-100'f32..100'f32) / 100'f32
+    result.weights[i] = rand(-1'f32..1'f32)
 
 proc feedForward(p: Perceptron, input: array[3, float32]): int32 =
   # Feedforward the input and get the output (1 or -1)
@@ -60,12 +60,8 @@ proc main =
   # --------------------------------------------------------------------------------------
   # Set up the raylib window
   setConfigFlags(flags(Msaa4xHint))
-  initWindow(screenWidth, screenHeight, "raylib - perceptron")
-  let camera = Camera2D(
-    # Move the origin to the center of the screen
-    offset: Vector2(x: screenWidth/2'f32, y: screenHeight/2'f32),
-    zoom: -1 # Flip both axis
-  )
+  initWindow(screenWidth, screenHeight, "raylib example - perceptron")
+  let camera = Camera2D(zoom: 1)
   randomize()
   var perceptron = initPerceptron(0.0001) # The perceptron with a learning rate of 0.0001
   var training: array[NumPoints, array[3, float32]] # An array for training data
@@ -82,8 +78,7 @@ proc main =
     # Update
     # ------------------------------------------------------------------------------------
     # Get the current (x, y) of the training data
-    let x = training[count][0]
-    let y = training[count][1]
+    let (x, y) = (training[count][0], training[count][1])
     # What is the desired output?
     let desired: int32 = if y > f(x): 1 else: -1
     # Train the perceptron
@@ -97,21 +92,22 @@ proc main =
       clearBackground(RayWhite)
       # Reorient the canvas to match a traditional Cartesian plane
       mode2D(camera):
-        # pushMatrix()
-        # # Translate the origin to the center of the screen
-        # translatef(screenWidth/2'f32, screenHeight/2'f32, 0)
-        # # Flip the y-axis by scaling it by -1.0
-        # scalef(1, -1, 1)
+        setCullFace(FaceFront)
+        pushMatrix()
+        # Translate the origin to the center of the screen
+        translatef(screenWidth/2'f32, screenHeight/2'f32, 0)
+        # Flip the y-axis by scaling it by -1.0
+        scalef(1, -1, 1)
         # Draw the line
         drawLine(Vector2(x: -screenWidth/2'f32, y: f(-screenWidth/2'f32)),
             Vector2(x: screenWidth/2'f32, y: f(screenWidth/2'f32)), 2, Black)
         # Draw all the points and color according to the output of the perceptron
         for dataPoint in training.items:
           let guess = perceptron.feedForward(dataPoint)
-          let pos = Vector2(x: dataPoint[0], y: dataPoint[1])
-          drawCircle(pos, 6, DarkGray)
-          drawCircle(pos, 4, if guess > 0: LightGray else: White)
-        # popMatrix()
+          let (x, y) = (dataPoint[0].int32, dataPoint[1].int32)
+          drawCircle(x, y, 6, DarkGray)
+          drawCircle(x, y, 4, if guess > 0: LightGray else: White)
+        popMatrix()
     # ------------------------------------------------------------------------------------
   # De-Initialization
   # --------------------------------------------------------------------------------------
