0bcfbaf47f2e063d06dafefd7f607ac2dce5622c
Antonis Geralis <b3lievem@yandex.com>
2024-06-13 21:57:43 +0300

    fix example

--- others/raymath_vector_angle.nim
+++ others/raymath_vector_angle.nim
@@ -42,42 +42,53 @@ proc main =
   while not windowShouldClose(): # Detect window close button or ESC key
     # Update
     # ------------------------------------------------------------------------------------
+    var startAngle: float32 = 0
+    case angleMode
+    of Angle:
+      startAngle = -lineAngle(v0, v1).radToDeg
+    of LineAngle:
+      startAngle = 0
+    v2 = getMousePosition()
     if isKeyPressed(Space):
       angleMode = not angleMode
     case angleMode
     of Angle:
+      if isMouseButtonDown(Right):
+        v1 = getMousePosition()
       # Calculate angle between two vectors, considering a common origin (v0)
-      v1 = v0 + Vector2(x: 100, y: 80)
-      v2 = getMousePosition()
       angle = angle(normalize(v1 - v0), normalize(v2 - v0)).radToDeg
     of LineAngle:
       # Calculate angle defined by a two vectors line, in reference to horizontal line
-      v1 = Vector2(x: screenWidth div 2, y: screenHeight div 2)
-      v2 = getMousePosition()
-      angle = lineAngle(v1, v2).radToDeg
+      angle = lineAngle(v0, v2).radToDeg
     # ------------------------------------------------------------------------------------
     # Draw
     # ------------------------------------------------------------------------------------
     beginDrawing()
     clearBackground(White)
-    if angleMode == Angle:
-      drawText("v0", v0.x.int32, v0.y.int32, 10, DarkGray)
-    drawText("v1", v1.x.int32, v1.y.int32, 10, DarkGray)
-    drawText("v2", v2.x.int32, v2.y.int32, 10, DarkGray)
     case angleMode
     of Angle:
       drawText("MODE: Angle between V1 and V2", 10, 10, 20, Black)
       drawLine(v0, v1, 2, Black)
       drawLine(v0, v2, 2, Red)
-      let startAngle = 90 - lineAngle(v0, v1).radToDeg
-      drawCircleSector(v0, 40, startAngle, startAngle + angle - 360*float32(angle > 180), 32, fade(Green, 0.6))
+      drawCircleSector(v0, 40, startAngle, startAngle + angle, 32, fade(Green, 0.6))
     of LineAngle:
       drawText("MODE: Angle formed by line V1 to V2", 10, 10, 20, Black)
       drawLine(0, screenHeight div 2, screenWidth, screenHeight div 2, LightGray)
-      drawLine(v1, v2, 2, Red)
-      drawCircleSector(v1, 40, 90, 180 - angle - 90, 32, fade(Green, 0.6))
+      drawLine(v0, v2, 2, Red)
+      drawCircleSector(v0, 40, startAngle, startAngle - angle, 32, fade(Green, 0.6))
+    drawText("v0", v0.x.int32, v0.y.int32, 10, DarkGray)
+    # If the line from v0 to v1 would overlap the text, move it's position up 10
+    if angleMode == Angle and (v0 - v1).y > 0:
+      drawText("v1", v1.x.int32, v1.y.int32-10, 10, DarkGray)
+    if angleMode == Angle and (v0 - v1).y < 0:
+      drawText("v1", v1.x.int32, v1.y.int32, 10, DarkGray)
+    # If angle mode 1, use v1 to emphasize the horizontal line
+    if angleMode == LineAngle:
+      drawText("v1", v0.x.int32+40, v0.y.int32, 10, DarkGray)
+    # position adjusted by -10 so it isn't hidden by cursor
+    drawText("v2", v2.x.int32-10, v2.y.int32-10, 10, DarkGray)
     drawText("Press SPACE to change MODE", 460, 10, 20, DarkGray)
-    drawText(&"ANGLE: {angle:2.2f}", 10, 40, 20, Lime)
+    drawText(&"ANGLE: {angle:2.2f}", 10, 70, 20, Lime)
     endDrawing()
     # ------------------------------------------------------------------------------------
   # De-Initialization
